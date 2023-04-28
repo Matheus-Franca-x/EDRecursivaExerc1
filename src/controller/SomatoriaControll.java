64a94c6e80919c6b6282bd8064f4800ce95d9aaf
Matheus <matheussss6613@gmail.com>
2023-04-28 04:09:10 -0300

    Fim

--- src/controller/SomatoriaControll.java
+++ src/controller/SomatoriaControll.java
@@ -9,19 +9,11 @@ public class SomatoriaControll
 	
 	
 	public int soma(int valor) //Inicio da somatoria
-	{
-		
-		if(valor <= 0) //caso o valor de entrada seja menor ou igual a zero, ele ira retornar o valor como zero no final
-		{
-			return 0;
-		}
-		else
-		{
-			return valor + soma(valor - 1); //nesse caso, quando o valor no final for igual a 0, ele ira retornar tudo
-		}									//exemplo: valor = 4, 4 + 3 + 2 + 1 + 0 = 10, esse sera o valor da somatoria no final.
-		
-		
-		
+	{		
+		return valor <= 0 ? 0 : valor + soma(valor - 1);
+		//caso o valor de entrada seja menor ou igual a zero, ele ira retornar o valor como zero no final
+		//nesse caso, quando o valor no final for igual a 0, ele ira retornar tudo
+		//exemplo: valor = 4, 4 + 3 + 2 + 1 + 0 = 10, esse sera o valor da somatoria no final.	
 	}
 	
 	
--- src/view/Main.java
+++ src/view/Main.java
@@ -8,7 +8,7 @@ public class Main {
 	{
 		SomatoriaControll somatoria = new SomatoriaControll(); //chamada da classe SomatoriaControll
 		
-		System.out.println(somatoria.soma(0)); //inicio do codigo para comecar a somatoria com um valor inicial
+		System.out.println(somatoria.soma(4)); //inicio do codigo para comecar a somatoria com um valor inicial
 		
 	}
 
